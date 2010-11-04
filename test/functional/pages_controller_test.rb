beeb4599f738f1b512fb1becfcbc5b76ce96edd8
Kamil Pluszczewicz <k.pluszczewicz@gmail.com>
2010-11-04 15:30:35 +0100

    Pierwszy raz

---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get Home" do
+    get :Home
+    assert_response :success
+  end
+
+  test "should get About" do
+    get :About
+    assert_response :success
+  end
+
+  test "should get Contact" do
+    get :Contact
+    assert_response :success
+  end
+
+end
